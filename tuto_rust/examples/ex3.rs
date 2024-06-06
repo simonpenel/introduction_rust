ef13e891c4d5d5c1ca004a32e0c1c156de53bcde
Simon Penel <simon.penel@univ-lyon1.fr>
2024-06-06 16:52:25 +0200

    Update examples

--- tuto_rust/examples/ex3.rs
+++ tuto_rust/examples/ex3.rs
@@ -1,26 +1,71 @@
 
 fn main() {
-    println!("Hello, world!"); 
+     println!("Hello, world!"); 
 
     // ==================================
-    // Les differentes variables
+    // Variables mutables et non mutables
     // ==================================
-
-    // Inference de type
-    let mut a = 21;
+    
+    let a = 32;
     println!("{}",a);
-    print_type_of(&a);
-    a = a + 10;
+
+    // cette instruction engendre une erreur:
+    // a = 42;
+
+    let mut b = 42;
+    println!("{}",b);
+
+    // cette instruction est correcte:
+    b = 42;
+    println!("{}",b);
+
+
+    // ==================================
+    // Le "borowing"
+    // ==================================
+
+    let hello = String::from("Hello, world!");
+
+    println!("{}",hello);
+
+    let coucou = hello;
+
+    println!("{}",coucou);     
+    // cette instruction engendre une erreur:
+    // println!("{}",hello);
+
+    // ==================================
+    // Les references
+    // ==================================
+
+    let bonjour = &coucou;
+    println!("{}",bonjour);
+    // println! recupere automatiquement
+    println!("{}",*bonjour);
+    println!("{}",coucou);     
+
+    // Le borowing ne pose pas de probleme avec les variable de type entier.
+    // La variable est copiée, cela ne coute pas cher, contrairement à une chaine de caracteres.
+
+    let hello = 1;
+    println!("{}",hello);
+    let coucou = hello;
+    println!("{}",coucou);     
+    // cette instruction marche:
+    println!("{}",hello);
+
+
+    // ==================================
+    // Inference de type
+    // ==================================    
+    let mut a = 21;
     println!("{}",a);
 
     // Genere une erreur
     // a = a + 12.0;
 
-    // Genere une erreur
-    //let a = 3 + 12.0;
 
     let mut b = 52.0;
-    print_type_of(&b);
     b = b + 10.0;
     println!("{}",b);
 
@@ -28,29 +73,50 @@ fn main() {
     b = b + 10.0;
     println!("{}",b);
 
-    // Specifier le type
-
+    // Specifier le type: ici un entier positif codé sur 32 bit : "u32"
     let a: u32 = 12;
-    let b = a * 2;
-    let c: u64 = 54;
-    // Genere une erreur    
-    //let d = a + c;
-
-    let chaine = "Bonjour chez vous";
-    println!("{}",chaine);
-    print_type_of(&chaine);
-
-    let chaine = String::from("Bonjour chez vous");
-    println!("{}",chaine);
-    print_type_of(&chaine);
 
 
-    let chaine = "Bonjour chez vous".to_string();
-    println!("{}",chaine);
-    print_type_of(&chaine);
+    // ==================================
+    // Les fonctions
+    // ==================================
+    let a: u32 = 12;
+    ma_fonction(a);
+
+    let hello = String::from("Hello, world!");
+    println!(" {}",hello);
+    ma_fonction_string(hello);
+    // Genere une erreur:
+    // println!(" {}",hello);
+
+    println!("avant {}",a);
+    ma_fonction_3(a);
+    println!("apres {}",a);
+    // on constate que rien n'a changé
+
+
 
 }
 
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
+fn ma_fonction(x:u32) {
+    println!("entree = {}", x)
+}
+
+fn ma_fonction_string(x:String) {
+    println!("entree = {}", x)
+}
+
+
+fn ma_fonction_2(x:u32) {
+    println!("entree = {}", x);
+    // Genere une erreur
+    //x = x + 5;
+
+}
+
+fn ma_fonction_3(mut x:u32) {
+    println!("entree = {}", x);
+    x = x + 5;
+    println!("nouvelle entree = {}", x);
+
 }
