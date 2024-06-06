7854da64cf6cc8540b78d5efa842d039e83452ec
Simon Penel <simon.penel@univ-lyon1.fr>
2024-06-06 15:15:52 +0200

    Update examples

--- tuto_rust/examples/ex1.rs
+++ tuto_rust/examples/ex1.rs
@@ -10,17 +10,21 @@ fn main() {
     let a = 32;
     println!("{}",a);
 
-  	// cette instruction engendre une erreur:
-   	// a = 42;
+    // cette instruction engendre une erreur:
+    // a = 42;
+
+    // cette instruction engendre une erreur:
+    // a = a + 10;
 
     let mut b = 42;
     println!("{}",b);
 
-  	// cette instruction est correcte:
-   	b = 42;
+    // cette instruction est correcte:
+    b = 42;
     println!("{}",b);
 
-
-
-
+    // cette instruction est correcte:
+    b = b + 10;
+    println!("{}",b);    
+        
 }
