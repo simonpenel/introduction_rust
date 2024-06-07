a57781cdc6a23fb4433c062b5effe19ba08f529e
Simon Penel <simon.penel@univ-lyon1.fr>
2024-06-07 16:20:44 +0200

    Update examples

---
+++ tuto_rust/examples/projet_agenda_5.rs
@@ -0,0 +1,84 @@
+// Implementer un trait pour une structure
+// =======================================
+
+#[derive(Debug)]
+pub enum JourSemaine {
+	Lundi,
+	Mardi,
+	Mercredi,
+	Jeudi,
+	Vendredi,
+	Samedi,
+	Dimanche
+}
+
+#[derive(Debug)]
+pub enum Mois {
+	Octobre,
+	Novembre,
+	Decembre,
+	Juin,
+	Juillet,
+	Aout,
+}
+
+#[derive(Debug)] 
+pub struct Jour {
+	/// jour de la semaine
+	jour_sem: JourSemaine,
+	/// jour du mois
+	jour_mois: u32,
+	/// mois
+	mois: Mois	
+	}
+	
+// Le trait "Display" (necessaire pour utiliser println!) n'existe pas pour notre structure.
+// Nous allons le definir	
+impl std::fmt::Display for Jour {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(f, "le {:?} {} {:?}",self.jour_sem, self.jour_mois, self.mois)
+    }
+ }
+ 
+pub struct Date {
+	/// jour du mois
+	jour_mois: u32,
+	/// mois
+	mois: Mois	
+	}
+impl std::fmt::Display for Date {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    	match self.mois {
+    	Mois::Octobre => write!(f, "le {} du mois d'{:?}", self.jour_mois, self.mois),
+    	Mois::Aout => write!(f, "le {} du mois d'{:?}", self.jour_mois, self.mois),
+    	_ =>  write!(f, "le {} du mois de {:?}", self.jour_mois, self.mois),
+    	}
+    }
+}
+// Implemente des fonctions pour la structure Date
+impl Date {
+    pub fn new(j: u32, m: Mois) -> Self {
+        Self {
+            jour_mois: j,
+            mois: m,
+        }
+    }
+    pub fn premier_jour() -> Self {
+        Self {
+            jour_mois: 1,
+            mois: Mois::Octobre,
+        }
+    }
+    pub fn affiche(&self) {
+    	println!("------ {:?}/{} --------",self.mois, self.jour_mois) 	
+   }
+    
+}
+fn main() {
+	let p = Date::premier_jour();
+	println!("Le premier jour de l'annee c'est {}",p);
+	let anniv = Date::new(2,Mois::Aout);
+	println!("L'anniversaire de Wes c'est {}",anniv);
+	anniv.affiche();
+}
+
