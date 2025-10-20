2074fc6202f5baf8e24e6da0ef7e90af5b6249c0
Simon Penel <simon.penel@univ-lyon1.fr>
2025-10-20 09:38:22 +0200

    Améliore ex1

--- tuto_rust/examples/ex1.rs
+++ tuto_rust/examples/ex1.rs
@@ -1,9 +1,12 @@
 
-// ==================================
-// VARIABLES MUTABLES ET NON MUTABLES
-// ==================================
+// =======================================================
+// VARIABLES MUTABLES ET NON MUTABLES, l'INFERENCE DE TYPE
+// =======================================================
     
 fn main() {
+    // ==================================
+    // variables mutables et non mutables
+    // ==================================
     println!("Hello, world!");
     let a = 32;
     println!("{}",a);
@@ -24,5 +27,25 @@ fn main() {
     // cette instruction est correcte:
     b = b + 10;
     println!("{}",b);
+
+    // ==================================
+    // L'inference de type
+    // ==================================
+    let mut a = 21;
+    println!("{}",a);
+    a = a + 5;
+    println!("{}",a);
+    // Genere une erreur
+    // a = a + 12.0;
+
+
+    let mut b = 52.0;
+    b = b + 10.0;
+    println!("{}",b);
+
+
+    // Specifier le type: ici un entier positif codé sur 32 bit : "u32"
+    let a: u32 = 12;    
+    println!("{}",a);
 
 }
