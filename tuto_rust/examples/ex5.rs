f6e658799afb31fb6da4efe09fcc5d83448abe5e
Simon Penel <simon.penel@univ-lyon1.fr>
2025-11-03 16:47:02 +0100

    Change ex5

--- tuto_rust/examples/ex5.rs
+++ tuto_rust/examples/ex5.rs
@@ -3,11 +3,12 @@
 // =========================
 //  LES TRAITS COPY ET CLONE
 // =========================
-
+// https://doc.rust-lang.org/reference/expressions.html#place-expressions-and-value-expressions
 //  Une fonction simple avec une chaîne
-pub fn ma_fonction_string_int(x: &u32) {
+pub fn ma_fonction_int_ref(x: &u32) {
     println!("entrée = {}", x);
     let y = *x; //ok
+    let z = *x; //ok
 }
 
 //  Une fonction simple avec une chaîne
@@ -19,7 +20,9 @@ pub fn ma_fonction_string_ref(x: &String) {
 fn main() {
     let hello = String::from("Hello");
     ma_fonction_string_ref(&hello);
+    println!("{}",hello);
 
     let hello = 12;
-    ma_fonction_string_int(&hello);
+    ma_fonction_int_ref(&hello);
+    println!("{}",hello);
 }
