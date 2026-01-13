71849803861dfb40ec1d34ea03eec94ee8e71193
Simon Penel <simon.penel@univ-lyon1.fr>
2026-01-13 11:02:51 +0100

    Commentaires en angalis

--- tuto_rust/examples/ex1.rs
+++ tuto_rust/examples/ex1.rs
@@ -1,62 +1,58 @@
 // ======================================================================
-// VARIABLES MUTABLES ET NON MUTABLES, LE SHADOWING,  l'INFERENCE DE TYPE
-// ...
 // MUTABLE AND IMMUTABLE VARIABLES, SHADOWING,  TYPE INFERENCE
 // ======================================================================
 
 fn main() {
     // ==================================
-    // variables mutables et non mutables
-    // ...
     // mutable and immutable variables
     // ==================================
     println!("Hello, world!");  // <-  an instruction ends with ';'.
-    let a = 32;            // <- declaration of a  variable with 'let'
+    let a = 32;                 // <- declaration of a  variable with 'let'
     println!("{}", a);          // <- println! is a rust  macro dedicated to displaying text
 
-    // cette instruction engendre une erreur:
+    // this instruction causes an error:
     // a = 42;
 
-    // cette instruction engendre une erreur:
+    // this instruction causes an error:
     // a = a + 10;
 
     let mut b = 32;
     println!("{}", b);
 
-    // cette instruction est correcte:
+    // this instruction is correct:
     b = 42;
     println!("{}", b);
 
-    // cette instruction est correcte:
+    // this instruction is correct:
     b = b + 10;
     println!("{}", b);
 
     // ==================================
-    // L'inférence de type
+    // Type inference
     // ==================================
     let mut a = 21;
     println!("{}", a);
     a = a + 5;
     println!("{}", a);
-    // Génére une erreur
+    // this instruction causes an error:
     // a = a + 12.0;
 
     let mut b = 52.0;
     b = b + 10.0;
     println!("{}", b);
 
-    // Spécifier le type: ici un entier positif ("unsigned") codé sur 32 bit : "u32"
+    // Specifying type: here a unsigned  integer encoded on 32 bits : "u32"
     let a: u32 = 12;
     println!("{}", a);
 
-    // Quelques types : i8,u8, i16, u16, i32, u32,f32,f64 ...
+    // Some types types : i8,u8, i16, u16, i32, u32,f32,f64 ...
     // https://doc.rust-lang.org/book/ch03-02-data-types.html
 
     // ============
-    // Le shadowing
+    // Shadowing
     // ============
     let a = 10;
-    //  a = a + 10 ; // Erreur
+    //  a = a + 10 ; // Error
     let a = a + 10; // "shadowing"
     println!("{}", a);
 }
