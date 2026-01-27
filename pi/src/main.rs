bd58d9ddf913d106feb00eead9595fea6360db22
Simon Penel <simon.penel@univ-lyon1.fr>
2026-01-27 12:56:15 +0100

    Update main.rs

--- pi/src/main.rs
+++ pi/src/main.rs
@@ -8,8 +8,12 @@ fn main() {
         Err(_e) => { panic!("Please enter an integer") }
     }; 
     while i < imax {
-        let val = (4 * i + 1) * (4 * i + 3);
-        somme = somme + 1.0 / val as f64;
+        // let val = (4 * i + 1) * (4 * i + 3);
+
+        let val1 = i.checked_mul(4).and_then(|v| v. checked_add(1)).unwrap();
+        let val2 = i.checked_mul(4).and_then(|v| v. checked_add(3)).unwrap();
+        let val3 = val1.checked_mul(val2).expect("Overflow!");
+        somme = somme + 1.0 / val3 as f64;
         i += 1;
     }
     println!("{}",somme * 8.0);
