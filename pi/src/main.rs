9377de6903b32b79ee51f36086b11fc58f45b1df
Simon Penel <simon.penel@univ-lyon1.fr>
2026-02-12 09:20:07 +0100

    Make pi example more simple

--- pi/src/main.rs
+++ pi/src/main.rs
@@ -3,11 +3,8 @@ fn main() {
     let mut somme = 0.0;
     let imax = 100000000;
     while i < imax {
-        // let val = (4 * i + 1) * (4 * i + 3);
-        let val1 = i.checked_mul(4).and_then(|v| v. checked_add(1)).unwrap();
-        let val2 = i.checked_mul(4).and_then(|v| v. checked_add(3)).unwrap();
-        let val3 = val1.checked_mul(val2).expect("Overflow!");
-        somme = somme + 1.0 / val3 as f64;
+        let val = (4 * i + 1) * (4 * i + 3);
+        somme = somme + 1.0 / val as f64;
         i += 1;
     }
     println!("{}",somme * 8.0);
